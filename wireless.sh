2e1b03748b6ff2594019b4feab6300bef4296d4b
Hjalmar S. Andersen <hjalmar@islandgarden.no>
2021-02-18 12:06:51 +0100

    added device check

--- wireless.sh
+++ wireless.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo Enter device IP
-read varname
+if [ -z "$1" ]; then
+    echo Enter device IP
+    read ip
+else 
+    ip=$1
+fi
+echo "Checking for Flutter devices..."
+DEVICE=$(flutter devices) # Error when piping flutter devices
+NODEV=$(echo $DEVICE | cut -c1-19)
+if [[ "$NODEV" == "No devices detected" ]]; then
+    echo $NODEV 
+    echo "Connect your device over USB first to connect wirelessly"
+    exit 1
+else
+    echo "Device found"
+fi
 adb tcpip 5555
-adb connect $varname
+adb connect $ip
