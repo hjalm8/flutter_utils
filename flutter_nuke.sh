dd3c6958fa134f89331358d6e584323e1c7ce134
Hjalmar S. Andersen <hjalmar@islandgarden.no>
2021-02-18 09:33:25 +0100

    prevent running nuke in non flutter dir

--- flutter_nuke.sh
+++ flutter_nuke.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Check if in parent of Flutter root
+test -f app/pubspec.yaml && echo "Moving to flutter dir app/" && cd app
+# Check if in subdir of flutter project, i.e. ios/, android/
+test -f ../pubspec.yaml && echo "Moving to parent dir ../" && cd ..
+# Check if in Flutter repo root
+test ! -f ./pubspec.yaml && echo "Not in flutter project" && exit 1
 echo "Cleaning..."
 flutter clean
 cd ios
